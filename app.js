d2bdbcb5b51c66535946c5fece79d8a5861874de
AvaCode69 <reacthmd34@gmail.com>
2023-01-20 01:05:49 +0100

    Submenu Setup

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import sublinks from "./data.js";
 const toggleBtn = document.querySelector(".toggle-btn");
 const closeBtn = document.querySelector(".close-btn");
 const sidebarWrapper = document.querySelector(".sidebar-wrapper");
-const sidebar = document.querySelector(".sidebar-link");
+const sidebar = document.querySelector(".sidebar-links");
 const linkBtns = [...document.querySelectorAll(".link-btn")];
 const submenu = document.querySelector(".submenu");
 const hero = document.querySelector(".hero");
@@ -16,4 +16,33 @@ toggleBtn.addEventListener("click", () => {
 });
 closeBtn.addEventListener("click", () => {
   sidebarWrapper.classList.remove("show");
+});
+// set sidebar
+sidebar.innerHTML = sublinks
+  .map((item) => {
+    const { links, page } = item;
+    return `<article >
+<h4>${page}</h4>
+<div class="sidebar-sublinks">
+${links
+  .map((link) => {
+    return `<a href="${link.url}"><i class="${link.icon}"></i>${link.label}</a>`;
+  })
+  .join("")}
+</div>
+</article>`;
+  })
+  .join("");
+
+linkBtns.forEach((btn) => {
+  btn.addEventListener("mouseover", function (e) {
+    const text = e.currentTarget.textContent;
+    const tempBtn = e.currentTarget.getBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+
+    submenu.classList.add("show");
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${bottom}px`;
+  });
 });
